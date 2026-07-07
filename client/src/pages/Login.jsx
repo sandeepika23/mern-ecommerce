84c76728de3b8eb24ee1852707ca1aafea265eeb
Sandeepika <sandeepikasettipalli3@gmail.com>
2026-07-07 11:14:34 +0530

    Fix missing useEffect import

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext,useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
 import "./Auth.css";
